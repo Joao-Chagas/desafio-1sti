5a1b84e7c6327491135b31e01315dc946bc3a46d
João Chagas <jpchagas@tasken.com>
2021-12-08 22:12:20 -0300

    feat: adiciona interface de repositorio de usuario

--- src/@types/user/IUserRepository.ts
+++ src/@types/user/IUserRepository.ts
@@ -2,7 +2,6 @@ type TUserRequest = import('@api/user/dto/request/User').UserRequest;
 type TUserResponse = import('@api/User/dto/response/User').UserResponse;
 
 declare interface IUserRepository {
-  getByCpf(cpf: string): Promise<TUserResponse>;
   getAll(): Promise<TUserResponse[]>;
   create(data: TUserRequest): Promise<TUserResponse>;
   update(id: number, data: TUserRequest): Promise<TUserResponse>;
