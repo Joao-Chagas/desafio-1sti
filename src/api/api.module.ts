a276eca02401eb2c992a6414e1393a8df94c3b6a
João Chagas <jpchagas@tasken.com>
2021-12-08 22:13:49 -0300

    feat: adiciona modulo da camada api

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DomainModule } from '@domain/domain.module';
 import { AddressResolver } from '@api/address/Address.resolver';
+import { UserResolver } from '@api/user/User.resolver';
+import { LoginResolver } from './login/login.resolver';
 
 @Module({
   imports: [DomainModule],
-  providers: [AddressResolver],
+  providers: [AddressResolver, UserResolver, LoginResolver],
 })
 export class ApiModule {}
